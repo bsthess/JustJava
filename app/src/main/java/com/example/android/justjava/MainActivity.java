5e9ae6cafaaf8a467da856f205e62cd250126b5f
Bsthess <bsthess@yahoo.gr>
2017-12-29 18:47:08 +0200

    added email sending when order button clicked, opens the email message window. fixed orientation change issues some minor bugs and fixes removed order summary message

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,6 +7,7 @@
 package com.example.android.justjava;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -20,13 +21,36 @@ import android.widget.Toast;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+
+//declaring static Strings for saved Bundle keynames
+
+
+    public static final String QUANTITY_NUMBER = "quantity";
+    public static final String DISPLAY_MESSAGE = "priceMessage";
+
+
     int quantity = 1;
+    String priceMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            // Restore value of members from saved state
+
+            quantity = savedInstanceState.getInt(QUANTITY_NUMBER);
+            priceMessage = savedInstanceState.getString(DISPLAY_MESSAGE);
+
+            displayQuantity(quantity);
+
+        }
+
+
     }
+
 
     /**
      * This method is called when the order button is clicked.
@@ -43,25 +67,45 @@ public class MainActivity extends AppCompatActivity {
         boolean hasChoco = choco.isChecked();
         int price = calculatePrice(hasCream, hasChoco);
         String priceMessage = createOrderSummary(nameText, price, hasCream, hasChoco);
-        displayMessage(priceMessage);
-
+        sendEmail(nameText, priceMessage);
 
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.putInt(QUANTITY_NUMBER, quantity);
+        savedInstanceState.putString(DISPLAY_MESSAGE, priceMessage);
+    }
 
     // Making the String for the display message
 
     private String createOrderSummary(String nameText, int price, boolean creamState, boolean chocoState) {
-        String addCream = getResources().getString(R.string.AddWhippedCream);
-        String addChoco = getResources().getString(R.string.Addchocolate);
-        String Name = getResources().getString(R.string.Name);
-        String Quantity = getResources().getString(R.string.Quantity);
-        String Total = getResources().getString(R.string.Total);
-        String Thankyou = getResources().getString(R.string.ThankYou);
+        String addCream = this.getResources().getString(R.string.WhippedCream);
+        String addChoco = this.getResources().getString(R.string.Chocolate);
+        String Name = this.getResources().getString(R.string.Name);
+        String Quantity = this.getResources().getString(R.string.Quantity);
+        String Total = this.getResources().getString(R.string.Total);
+        String ThankYou = this.getResources().getString(R.string.ThankYou);
 
-        String priceMessage = Name + nameText + "\n" + addCream + creamState + "\n" + addChoco + chocoState + "\n" + Quantity + quantity + "\n" + Total + price + "$" + "\n" + Thankyou;
-        return priceMessage;
+        if (creamState & chocoState) {
+            priceMessage = Name + nameText + "\n" + addCream + "\n" + addChoco + "\n" + Quantity + quantity + "\n" + Total + price + "$" + "\n" + ThankYou;
+            return priceMessage;
+        }
+        if (creamState & !chocoState) {
+            priceMessage = Name + nameText + "\n" + addCream + "\n" + Quantity + quantity + "\n" + Total + price + "$" + "\n" + ThankYou;
+            return priceMessage;
+        }
 
+        if (!creamState & chocoState) {
+            priceMessage = Name + nameText + "\n" + addChoco + "\n" + Quantity + quantity + "\n" + Total + price + "$" + "\n" + ThankYou;
+            return priceMessage;
+        } else {
+            priceMessage = Name + nameText + "\n" + Quantity + quantity + "\n" + Total + price + "$" + "\n" + ThankYou;
+            return priceMessage;
+        }
 
     }
 
@@ -84,12 +128,7 @@ public class MainActivity extends AppCompatActivity {
         return price;
     }
 
-    //display the message for the order
-    public void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
 
-    }
 
     /**
      * The method that increments quantity
@@ -102,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
             Context context = getApplicationContext();
             CharSequence text = "Maximum order is 100 coffees!";
             int duration = Toast.LENGTH_SHORT;
+
 // the toast message that informs that we can not take an order bigger than 100 coffees
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
@@ -121,6 +161,7 @@ public class MainActivity extends AppCompatActivity {
             Context context = getApplicationContext();
             CharSequence text = "Minimum order is 1 coffee!";
             int duration = Toast.LENGTH_SHORT;
+
 // the toast message that informs that we can not take an order bigger than 100 coffees
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
@@ -138,5 +179,17 @@ public class MainActivity extends AppCompatActivity {
     private void displayQuantity(int num) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + num);
+    }
+
+
+    public void sendEmail(String nameOfOrder, String content) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Order for " + nameOfOrder);
+        intent.putExtra(Intent.EXTRA_TEXT, content);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
